3e44a3a7e7b787b3312b9e714a110fe8e79b1f53
timofeyluchkin <pradrobieco@gmail.com>
2023-12-13 23:11:11 +0300

    GameTypeWindow ready

---
+++ Views/GameTypeWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace cinequiz.Views;
+
+public partial class GameTypeWindow : Window
+{
+    public GameTypeWindow()
+    {
+        InitializeComponent();
+    }
+}
